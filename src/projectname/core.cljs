7c81248031fa635ab3eb764839d87d68f608eb74
Steffen <steffen@brennscheidt.net>
2021-03-31 20:17:16 +0200

    initial commit, running `yarn install` and then `yarn shadow-cljs watch app` leads to:
       6 | (rum/defc hello-world [testa]
       7 |   [:div
       8 |    [:h2 testa]
    ---------------^----------------------------------------------------------------
    Use of undeclared Var projectname.core/testa

---
+++ src/projectname/core.cljs
@@ -0,0 +1,27 @@
+(ns projectname.core
+  (:require [rum.core :as rum]))
+
+(defonce app-state (atom {:text "Hello world!"}))
+
+(rum/defc hello-world [testa]
+  [:div
+   [:h2 testa]
+   [:h1 (:text @app-state)]
+   [:h3 "Edit this and watch it change!"]])
+
+(defn start []
+  ;; start is called by init and after code reloading finishes
+  ;; this is controlled by the :after-load in the config
+  (rum/mount (hello-world "Hello from Rum")
+             (. js/document (getElementById "app"))))
+
+(defn ^:export init []
+  ;; init is called ONCE when the page loads
+  ;; this is called in the index.html and must be exported
+  ;; so it is available even in :advanced release builds
+  (start))
+
+(defn stop []
+  ;; stop is called before any code is reloaded
+  ;; this is controlled by :before-load in the config
+  (js/console.log "stop"))
